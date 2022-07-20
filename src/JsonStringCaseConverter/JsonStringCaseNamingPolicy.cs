54728c5e8b5f6e191d22f354e23a59ff0a5f46f6
Claus Stolz <clausstolz@yandex.ru>
2022-07-20 17:26:55 +0400

    JSCC-5 fixed bugs and improved splitting strings into words

--- src/JsonStringCaseConverter/JsonStringCaseNamingPolicy.cs
+++ src/JsonStringCaseConverter/JsonStringCaseNamingPolicy.cs
@@ -2,15 +2,33 @@ using System.Text.Json;
 
 namespace JsonStringCaseConverter;
 
+/// <summary>
+/// Improved <see cref="JsonNamingPolicy"/> class to easily
+/// implement case conversion.
+/// </summary>
 public class JsonStringCaseNamingPolicy : JsonNamingPolicy
 {
     private readonly StringCases _stringCase;
 
+    /// <summary>
+    /// Constructor that takes as an argument the rule needed
+    /// for case conversion.
+    /// </summary>
+    /// <param name="stringCase">
+    /// An enumeration element denoting the desired case type.
+    /// </param>
     public JsonStringCaseNamingPolicy(StringCases stringCase)
     {
         _stringCase = stringCase;
     }
     
+    /// <summary>
+    /// Converts a value based on the the specified case type
+    /// according to the policy.
+    /// </summary>
+    /// <param name="name">
+    /// The value to convert.
+    /// </param>
     public override string ConvertName(string name) =>
         _stringCase switch
         {
